9dd122ff9398b75a9e48e8047d2fe11640d3e7d3
Lucas Cauã Sousa <70662776+LCS-TECH@users.noreply.github.com>
2024-04-25 16:00:52 -0300

    Update settings.py
    ALLOWED HOSTS

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-p%a7njmpp-*sj*-g$k!-(olb3%0(y&bk=y34g!!)e993whp6w*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
